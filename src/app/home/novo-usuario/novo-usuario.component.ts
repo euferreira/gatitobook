d0e0a9955cad6dee6204244b62fd8e184a987027
Fernando Ferreira <fernandferreira@hotmail.com>
2022-02-04 11:01:56 -0300

    feat: Implementação das rotas de login

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { NovoUsuario } from './novo-usuario';
-import { NovoUsuarioService } from './novo-usuario.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {NovoUsuario} from './novo-usuario';
+import {NovoUsuarioService} from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -11,17 +11,15 @@ import { NovoUsuarioService } from './novo-usuario.service';
 export class NovoUsuarioComponent implements OnInit {
   novoUsuarioForm!: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
-  ) {}
+  constructor(private formBuilder: FormBuilder, private novoUsuarioService: NovoUsuarioService) {
+  }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email : [''],
-      fullName : [''],
-      userName : [''],
-      password : ['']
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: ['']
     });
   }
 
